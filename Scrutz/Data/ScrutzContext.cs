47671195960391bc0fc0ccc9121de376d267e7bc
TEE-JAY12 <akinyemitj3@gmail.com>
2023-04-18 15:08:09 +0100

    Added the Campaign database

---
+++ Scrutz/Data/ScrutzContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Scrutz.Model;
+using System.Xml;
+
+namespace Scrutz.Data
+{
+    public class ScrutzContext : DbContext
+    {
+        public ScrutzContext(DbContextOptions<ScrutzContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Campaign> Campaigns { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Campaign>().HasKey(p => p.Id);
+            modelBuilder.Entity<Campaign>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+            modelBuilder.Entity<Campaign>()
+                .Property(e => e.LinkedKeywords)
+                .HasMaxLength(300)
+                .HasConversion(
+                    v => string.Join(',', v),
+                    v => v.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                          .ToArray()
+                );
+        }
+
+    }
+}
---
+++ Scrutz/Model/Campaign.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Scrutz.Model
+{
+    public class Campaign
+    {
+        public int Id { get; set; }
+        public string? CampaignName { get; set;}
+        public string? CampaignDescription { get; set;}
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set;}
+        public bool? RecieveDailyDigest { get; set; }
+
+        [MaxLength(100)]
+        public string[]? LinkedKeywords { get; set; }
+        public DateTime? DailyDigestTime { get; set; }
+
+    }
+}
